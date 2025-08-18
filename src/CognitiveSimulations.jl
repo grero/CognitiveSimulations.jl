acb71553da0965c21a7aed82ae1fbb43e8e53db8
Roger Heirkstad <roger.herikstad@gmail.com>
2025-08-18 08:59:08 +0800

    Refactor with plotting implemented in extension

--- src/CognitiveSimulations.jl
+++ src/CognitiveSimulations.jl
@@ -6,13 +6,11 @@ using JLD2
 using Random
 using StableRNGs
 using StatsBase
-using MultiDimensionalTimeSeriesPlots
 using Glob
-using Makie
 
 include("subspace.jl")
 include("training.jl")
-include("random_sequence_task.jl")
+#include("random_sequence_task.jl")
 include("plots.jl")
 
 end # module
