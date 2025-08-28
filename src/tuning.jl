0520d29695c1fcd0ba63830224855bfcdec89910
Roger Heirkstad <roger.herikstad@gmail.com>
2025-08-28 09:50:04 +0800

    Add functions for computing path length tuning

--- src/tuning.jl
+++ src/tuning.jl
@@ -170,3 +170,97 @@ function estimate_view_by_place_tuning_interaction(trialstruct::RNNTrialStructur
     end
     hh
 end
+
+function estimate_path_length_tuning_2(trialstruct::RNNTrialStructures.NavigationTrial{T}, h::Matrix{T}, position::AbstractArray{T,3}) where T <: Real
+    nt = size(position,3)
+    # compute path length from position
+    path_length = cumsum([zeros(T,1,nt);sqrt.(dropdims(sum(abs2, diff(position, dims=2),dims=1),dims=1))],dims=1)
+
+    # alternatively, simply 
+    # compute occupancy histogram
+    hhb = fit(Histogram, path_length[:])
+    # compute visited histogram
+    hh = fit(Histogram, path_length[:], weights(h[:]))
+
+    hh.edges[1], hh.weights./hhb.weights
+end
+
+function get_path_length(trial::RNNTrialStructures.NavigationTrial{T}, position::AbstractArray{T,3};invalid=zero(T)) where T <: Real
+     nt = size(position,3)
+    path_length = T[]
+    idx = CartesianIndex{2}[]
+    for i in 1:nt
+        idxe = findlast(dropdims(maximum(position[:,:,i],dims=1),dims=1) .> invalid)
+        pl = cumsum(sqrt.(dropdims(sum(abs2, diff(position[:,1:idxe,i], dims=2),dims=1),dims=1)),dims=1)
+        append!(path_length, pl)
+        for j in 2:idxe
+            push!(idx, CartesianIndex((j,i)))
+        end
+    end
+    path_length, idx
+end
+
+function estimate_path_length_tuning(trialstruct::RNNTrialStructures.NavigationTrial{T}, h::Matrix{T}, position::AbstractArray{T,3},idxe::AbstractVector{Int64}) where T <: Real
+    nt = length(idxe)
+    path_length = T[]
+    hh = T[]
+    for i in 1:nt
+        pl = cumsum(sqrt.(dropdims(sum(abs2, diff(position[:,1:idxe[i],i], dims=2),dims=1),dims=1)),dims=1)
+        append!(path_length, pl)
+        append!(hh, h[2:idxe[i],i])
+    end
+    estimate_path_length_tuning(hh, path_length)
+end
+
+function estimate_path_length_tuning(hh::Vector{T}, path_length::Vector{T}) where T <: Real
+    # fit sigmoid
+    func(abc) = sum(abs2, scaled_sigmoid.(path_length, abc...) - hh)
+
+    q =optimize(func, ones(T,3))
+    yq = scaled_sigmoid.(path_length, q.minimizer...)
+    rss = sum(abs2, yq .- hh)
+    rss_shuffled = [sum(abs2, yq - shuffle(hh)) for i in 1:1000]
+    path_length, hh, q, rss, percentile(rss_shuffled, 1)
+end
+
+function estimate_path_length_tuning(trialstruct::RNNTrialStructures.NavigationTrial{T}, h::Array{T,3}, position::AbstractArray{T,3}) where T <: Real
+    path_length, idxf = get_path_length(trialstruct, position)
+    tuning_strength = zeros(T, size(h,1))
+    params = zeros(T, 3, size(h,1))
+    for i in 1:length(tuning_strength)
+        pl, hh, q, rss, rss_shuffled = estimate_path_length_tuning(trialstruct, h[i,idxf],path_length)
+        params[:,i] = q.minimizer
+        tuning_strength[i] = one(T) - rss/rss_shuffled
+    end
+    tuning_strength, params
+end
+
+"""
+    get_step_index(y::AbstractArray{T,3}) where T <: Real
+
+Get a flat index over the last 2 dimensions of y with only valid steps
+"""
+function get_step_index(y::AbstractArray{T,3};invalid=zero(T)) where T <: Real
+    nt = size(y,3)
+    idx = CartesianIndex{2}[]
+    for i in 1:nt
+        idxe = findlast(dropdims(maximum(y[:,:,i],dims=1),dims=1) .> invalid)
+        for ii in 1:idxe
+            push!(idx, CartesianIndex((ii,i)))
+        end
+    end
+    idx
+end
+
+function predict_path_length(trialstruct::RNNTrialStructures.NavigationTrial{T}, h::AbstractArray{T,3}, position::AbstractArray{T,3}) where T <: Real
+    path_length,idxf = CognitiveSimulations.get_path_length(trialstruct, position)
+    X = h[:,idxf]
+    train_idx = shuffle(1:length(idxf))[1:round(Int64, 0.8*length(idxf))]
+    sort!(train_idx)
+    test_idx = setdiff(1:length(idxf),train_idx)
+    pca = fit(PCA, X[:,train_idx])
+    Xp = predict(pca, X)
+    lq = LinearRegressionUtils.llsq_stats(permutedims(Xp[:,train_idx]), repeat(path_length[train_idx],1,1))
+    yq = lq.β[1:end-1,:]'*Xp[:,test_idx] .+ lq.β[1,1]
+    path_length[test_idx], yq
+end
