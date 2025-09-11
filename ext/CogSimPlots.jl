1aa4dceadb4e3d8161f6e55ce71824147249608e
Roger Heirkstad <roger.herikstad@gmail.com>
2025-09-12 06:27:43 +0800

    Implement dynamic change of p_stay in simulation

--- ext/CogSimPlots.jl
+++ ext/CogSimPlots.jl
@@ -64,14 +64,21 @@ function CognitiveSimulations.animate_task(arena::RNNTrialStructures.AbstractAre
     θ = T(π/2)
     ipos = Observable(((i,j), θ))
     do_pause = Observable(false)
+    p_stay_dynamic = p_stay
     on(tt) do _tt
         _ipos = ipos[]
         _θ = _ipos[2]
         i,j = _ipos[1]
-        Δθ = RNNTrialStructures.get_head_direction(fov/2, _θ;p_stay=p_stay)
+        Δθ = RNNTrialStructures.get_head_direction(fov/2, _θ;p_stay=p_stay_dynamic)
         _θ += Δθ
-        (i,j) = RNNTrialStructures.get_coordinate(i,j,arena, _θ;p_hd=p_hd)
-
+        (i1,j1) = RNNTrialStructures.get_coordinate(i,j,arena, _θ;p_hd=p_hd)
+        if abs(i1-i) + abs(j1-j) == 0
+            # reduce the probability of staying
+            p_stay_dynamic = max(zero(T), p_stay_dynamic*0.5f0)
+        else
+            p_stay_dynamic = p_stay
+        end
+        i,j = (i1,j1)
         ipos[] = ((i,j),_θ)
     end
 
